6d677cf01a9cb8b979e31171b3b26696e2ac0dbd
Martin Fabbri <martin.fabbri.a@gmail.com>
2018-04-19 01:45:22 -0700

    Adding test component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNewComponentComponent
   ],
   imports: [
     BrowserModule
